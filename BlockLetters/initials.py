6749ff61b3b26e2f3e346a9273feabaad59686b2
Sarah <sarah.carlock86@gmail.com>
2024-07-24 11:21:08 -0500

    Create Block Letters

---
+++ BlockLetters/initials.py
@@ -0,0 +1,50 @@
+# my name is Sarah Carlock
+# I'm living my best life
+first_initial = """
+ SSS
+S   S
+S   
+ SSS
+    S
+S   S
+ SSS
+"""
+second_initial = """
+ CCC
+C   C
+C
+C
+C
+C   C
+ CCC
+"""
+initials = first_initial+second_initial
+print(initials)
+
+### Example
+print("  SSS   L     ")
+print(" S   S  L     ")
+print(" S      L     ")
+print("  SSS   L     ")
+print("     S  L     ")
+print(" S   S  L     ")
+print("  SSS   LLLLL ")
+
+### Example
+print("      *     *                    ")
+print("                      *          ")
+print("*         *                 *    ")
+print("     *           *     *         ")
+print("           HHHHHHH        *      ")
+print(" *         HHHHHHH               ")
+print("     *   HHHHHHHHHHH    *     *  ")
+print("         *   @ @   *             ")
+print("        *     ^     *            ")
+print("  *       *       *         *    ")
+print("         SSS * * S               ")
+print("         * SSSSSSSS *            ")
+print("       *      O   SS  *          ")
+print("      *       O    S   *         ")
+print("      *       O        *         ")
+print("       *              *          ")
+print("           *   *   *             ")
