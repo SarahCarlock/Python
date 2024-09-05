3244b88d232088e6bc74437ac80e16da13df7bef
Sarah <sarah.carlock86@gmail.com>
2024-09-05 17:16:01 -0500

    Back on the bandwagon and learning python

---
+++ MatchCaseStatements/matchCaseStatments.py
@@ -0,0 +1,14 @@
+match expression:  
+    case value_1:  
+        # code to execute when expression equals value_1  
+    case value_2:  
+        # code to execute when expression equals value_2  
+    case value_3:  
+        # code to execute when expression equals value_3  
+    case value_4:  
+        # code to execute when expression equals value_4  
+    case value_N:  
+        # code to execute when expression equals value_N  
+    case default:  
+        # code to execute when expression isn't equal to any of the values  
+        
